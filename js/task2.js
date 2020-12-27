3072503e890e1e68489d2a335c03e0b60f91eaa5
Nata-user <70835039+Nata-user@users.noreply.github.com>
2020-12-27 11:12:28 +0200

    1

--- js/task2.js
+++ js/task2.js
@@ -1,12 +1,6 @@
 const countProps = function(obj) {
-  const keys = Object.keys(obj);
-  let i=0;
-  for (const key of keys) {
-    if (typeof obj[key] !== 'number') continue;
-   obj[key] += i;
-   return keys.length;}
-   if (typeof obj[{}] !== 'null');
-   return 0; 
+ const key= Object.keys(obj).length; 
+ return key;
 };
 console.log(countProps({})); // 0
 console.log(countProps({ name: 'Mango', age: 2 })); // 2
